97615a0d0f1662ad1df12703469d4f914daaac01
Гаджи Кадиев < >
2017-05-03 13:46:27 +0300

    123

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	public GameObject settings;
+	public void StartGame()
+	{
+		Application.LoadLevel (1);
+		//GameObject.DontDestroyOnLoad (gameObject); //Переноска из одной сцены в другую.
+	}
+
+	public void LoadGame()
+	{
+
+	}
+
+	public void Settings()
+	{
+		settings.SetActive (!settings.activeSelf);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+
+	public void setMusic(float value)
+	{
+		Global.music = value;
+	}
+
+	public void setSound(float value)
+	{
+		Global.music = value;
+	}
+}
